9f2287a989fee7b9e77ddb36c7d5404cc77bd99e
cdwyer-cognito <chris.dwyer@cognitoiq.com>
2019-03-01 19:05:54 +0000

    .

--- src/pages/Playpen/Playpen.jsx
+++ src/pages/Playpen/Playpen.jsx
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import SignatureCapture from '../../components/SignatureCapture/SignatureCapture';
-
 class Playpen extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
-  render() {
-    return <SignatureCapture watermark="Hello World" saveAs="svg" />;
-  }
+  render() {}
 }
 
 Playpen.propTypes = {};
